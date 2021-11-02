06076af5ff320d6c4f166ceeabe328e8aa5b4867
Sega5823 <sega9019@gmail.com>
2021-11-02 21:46:34 +0300

    Version 0.01

--- src/CountWords.java
+++ src/CountWords.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CountWords {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите предложение");
+        String text = in.nextLine();
+        String[] arrText = text.split(" ");
+        System.out.println("В вашем предложении количество слов равно " + arrText.length);
     }
 }
